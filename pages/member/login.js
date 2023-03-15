237db1e468225368840dded22f37e9b6e55e0875
JaeSeong.Song <zzyzigy@gmail.com>
2023-03-15 11:08:11 +0900

    MariaDB Member 클래스 구현 5 - NextAuth 7 - 로그인시 페이지 리다이렉션

--- pages/member/login.js
+++ pages/member/login.js
@@ -1,6 +1,20 @@
 import {useState} from "react";
 import {handleInput} from "../../models/Utils";
-import {signIn, useSession} from "next-auth/client";
+import {getSession, signIn, useSession} from "next-auth/client";
+
+export async function getServerSideProps(ctx) {
+
+    // 세션 객체 가져오기
+    const sess = await getSession(ctx);
+    if (sess) { // 로그인한 경우 회원정보로 이동
+        return {
+            redirect: {permanent: false, destination: '/member/myinfo'},
+            props: {}
+        }
+    }
+    return { props: {} }
+
+}
 
 export default function Login() {
 
@@ -14,12 +28,25 @@ export default function Login() {
         const data = {userid: userid, passwd: passwd};
 
         // signIn(인증시 활용할 Credentials id, 인증시 사용할 정보)
-        const res = await signIn('userid-passwd-credentials', {
+        //const res = await signIn('userid-passwd-credentials', {
+        const {error} = await signIn('userid-passwd-credentials', {
             userid, passwd,
-            redirect: true
+            redirect: false
         });
 
-        console.log('pg login -', await res.status);
+        // res : 인증성공여부를 http 상태 코드로 알려줌
+        // 인증성공 : 200, 실패 : 401
+        // console.log('pg login -', await res.status);
+
+        // error : 인증성공여부를 error로 알려줌
+        // 인증성공 : null, 실패 : CredentialsSignin
+        console.log('pg login -', await error);
+
+        if (error) { // 에러 발생시 - 인증 실패시
+            location.href = '/member/failogin';
+        } else {
+            location.href = '/member/myinfo';
+        }
     };
 
   return (
